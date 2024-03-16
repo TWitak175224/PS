414913444b2fc59c8a9ffd3141ae08c2359dea13
TWitak175224 <161207572+TWitak175224@users.noreply.github.com>
2024-03-16 13:34:09 +0100

    dodano C2

---
+++ C2/Z2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int sgn(double x){
+    if (x<0){
+        return -1;
+    }
+    else if(x>0){
+        return 1;
+    }
+    else
+        return 0;
+}
+double namniejsza(double x,double y,double z){
+    if x
+}
+
+
+int main(void) {
+    z21();
+
+    return 0;
+}
---
+++ C2/Z3.c
@@ -0,0 +1,3 @@
+//
+// Created by local on 16.03.2024.
+//
